f56a22c94ffcb8b68631e941decda2be071a4d02
obedkabwe <kabweobed@gmail.com>
2022-05-25 20:50:38 -0500

    created class Oder

---
+++ src/entities/Order.java
@@ -0,0 +1,14 @@
+package entities;
+
+import java.util.Date;
+
+import entities.enums.OderStatus;
+
+public class Order {
+
+	private Integer id;
+	private Date moment;
+	private OderStatus status;
+	
+	
+}
